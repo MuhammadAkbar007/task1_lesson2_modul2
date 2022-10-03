9fb90b0c5adc6b9a60061abe3eab57f480216ef4
MuhammadAkbar <fotismenos007@mail.ru>
2022-10-03 21:23:00 +0500

    Storm is coming

--- src/main/java/uz/pdp/task1_lesson2_modul2/entity/Attachment.java
+++ src/main/java/uz/pdp/task1_lesson2_modul2/entity/Attachment.java
@@ -17,7 +17,10 @@ public class Attachment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String fileOriginalName;
+
     private long size;
+
     private String contentType;
 }
